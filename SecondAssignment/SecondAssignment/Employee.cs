269e9ad1ff34f16230ee491226c2b1492328d889
Ranjan-Cotiviti <144988408+Ranjan-Cotiviti@users.noreply.github.com>
2023-09-19 12:16:40 +0545

    Add files via upload

---
+++ SecondAssignment/SecondAssignment/Employee.cs
@@ -0,0 +1,61 @@
+﻿//using System;
+//using System.Collections.Generic;
+//using System.Linq;
+//using System.Text;
+//using System.Threading.Tasks;
+
+//namespace SecondAssignment
+//{
+//    public class Employee
+//    {
+//        public string FirstName { get; set; }
+//        public string LastName { get; set; }
+//        public string Email { get; set; }
+//        public string Phone { get; set; }
+//        public int Age { get; set; }
+//        public string JobTitle { get; set; }
+//        public int YearsOfExperience { get; set; }
+//        public decimal Salary { get; set; }
+//        public string Department { get; set; }
+//    }
+
+//}
+
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Assignment2
+{
+    internal class Employee
+    {
+        //  First Name, Last Name,Email,Phone,Gender,Age,Job Title, Years Of Experience, Salary, Department
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public string Email { get; set; }
+        public string Phone { get; set; }
+        public string Gender { get; set; }
+        public string Age { get; set; }
+        public string JobTitle { get; set; }
+        public string YoE { get; set; }
+        public string Salary { get; set; }
+        public string Department { get; set; }
+        public Employee() { }
+        public Employee(string firstName, string lastName, string email, string phone, string gender, string age, string job, string yoE, string salary, string department)
+        {
+            FirstName = firstName;
+            LastName = lastName;
+            Email = email;
+            Phone = phone;
+            Gender = gender;
+            Age = age;
+            JobTitle = job;
+            YoE = yoE;
+            Salary = salary;
+            Department = department;
+        }
+    }
+}
---
+++ SecondAssignment/SecondAssignment/Program.cs
@@ -0,0 +1,94 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Threading.Tasks;
+
+
+namespace Assignment2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+           
+            List<Employee> employees = new List<Employee>();
+            var csvFileText = File.ReadAllLines(@"C:\Users\ranjan.goit\Downloads\employees.csv").ToList();
+            foreach (var csvEmployee in csvFileText
+                .Select((x, i) => new { Value = x, Index = i }))
+
+            {
+                if (csvEmployee.Index == 0)
+                {
+                    continue;
+                }
+                string[] values = csvEmployee.Value.Split(',');
+                if (values.Length == 10)
+                {
+                    Employee employee = new Employee
+                    {
+                        FirstName = values[0],
+                        LastName = values[1],
+                        Email = values[2],
+                        Phone = values[3],
+                        Gender = values[4],
+                        Age = values[5],
+                        JobTitle = values[6],
+                        YoE = values[7],
+                        Salary = values[8],
+                        Department = values[9]
+                    };
+                    employees.Add(employee);
+                }
+                else
+                {
+                    Console.WriteLine("Employee columns mismatch");
+                }
+
+            }
+            var groupByDepartment = employees.GroupBy(e => e.Department);
+            foreach (var group in groupByDepartment)
+            {
+                Console.WriteLine($"Department:  {group.Key}");
+                foreach (var employee in group)
+                {
+                    Console.WriteLine($"{employee.FirstName} {employee.LastName} | {employee.JobTitle} | {employee.Department} ");
+                }
+                Console.WriteLine();
+
+            }
+            Console.WriteLine();
+
+
+            var projectManager = employees.Where(e => e.JobTitle == "Project Manager")
+                .OrderByDescending(e => e.Salary)
+                .FirstOrDefault();
+            Console.WriteLine($"Highest salary earning Project Manager is {projectManager.FirstName} {projectManager.LastName} with salary {projectManager.Salary}");
+            Console.WriteLine();
+
+            var webDeveloper = employees.Where(e => e.JobTitle == "Web Developer")
+                .OrderByDescending(e => e.YoE).FirstOrDefault();
+            Console.WriteLine($"Most Experienced web Developer is {webDeveloper.FirstName} {webDeveloper.LastName} with {webDeveloper.YoE} years of experience");
+            Console.WriteLine();
+
+            var jobGroup = employees.GroupBy(e => e.JobTitle);
+            foreach (var group in jobGroup)
+            {
+                string jobTitle = group.Key;
+                decimal averageSalary = group.Average(e => (decimal.Parse(e.Salary)));
+
+                Console.WriteLine($"Job Title: {jobTitle}");
+                Console.WriteLine($"Average Salary: {averageSalary}");
+                Console.WriteLine();
+            }
+
+            var employeeCount = employees.GroupBy(e => e.Gender);
+            foreach (var group in employeeCount)
+            {
+                var count = group.Count();
+                Console.WriteLine($"Number of {group.Key} Employees: {count} ");
+            }
+        }
+
+    }
+}
